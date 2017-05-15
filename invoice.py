16b23a3c4db9eb5999fcd1c0e8399d028cc42edb
Luciano Rossi <lukio@gcoop.coop>
2017-05-15 15:28:17 -0300

    Add fields into a new page. (invoice_form)

--- invoice.py
+++ invoice.py
@@ -3,7 +3,6 @@
 # this repository contains the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond.pyson import Eval
 
 __all__ = ['Invoice']
 
@@ -25,26 +24,3 @@ class Invoice:
         if self.party:
             self.arba_retention = self.party.arba_retention
             self.arba_perception = self.party.arba_perception
-
-    #@classmethod
-    #def view_attributes(cls):
-    #    return super(Invoice, cls).view_attributes() + [
-    #        ('//[field="arba_perception"]', 'states', {
-    #                'invisible': Eval('type').in_(['in_invoice', 'in_credit_note']),
-    #                }),
-    #        ('//[field="arba_retention"]', 'states', {
-    #                'invisible': Eval('type').in_(['out_invoice', 'out_credit_note']),
-    #                }),
-    #        ]
-
-    @classmethod
-    def view_attributes(cls):
-        states_in = {'invisible': Eval('type').in_(['in_invoice', 'in_credit_note'])}
-        states_out = {'invisible': Eval('type').in_(['out_invoice', 'out_credit_note'])}
-        #states = {'invisible': True}
-        return super(Invoice, cls).view_attributes() + [
-            ('/form//group[@id="arba_perception"]', 'states', states_in),
-            #('/form//field[@name="arba_perception"]', 'states', states_in),
-            ('/form//group[@id="arba_retention"]', 'states', states_out),
-            #('/form//field[@name="arba_retention"]', 'states', states_out),
-            ]
