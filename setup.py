c4b8f918cc4e91ed9c2475616d1c09a33957fc27
Adrián Bernardi <adrian.bernardi@gmail.com>
2023-09-26 23:01:12 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,24 +7,39 @@ import io
 import os
 import re
 from configparser import ConfigParser
-from setuptools import setup
+from setuptools import find_packages, setup
+
+MODULE = 'account_arba'
+PREFIX = 'trytonar'
+MODULE2PREFIX = {
+    'account_ar': 'trytonar',
+    'account_retencion_ar': 'trytonar',
+    }
 
 
 def read(fname):
-    return io.open(
+    content = io.open(
         os.path.join(os.path.dirname(__file__), fname),
         'r', encoding='utf-8').read()
+    content = re.sub(
+        r'(?m)^\.\. toctree::\r?\n((^$|^\s.*$)\r?\n)*', '', content)
+    return content
 
 
 def get_require_version(name):
-    require = '%s >= %s.%s, < %s.%s'
+    if name in LINKS:
+        return ''  # '%s @ %s' % (name, LINKS[name])
+    if minor_version % 2:
+        require = '%s >= %s.%s.dev0, < %s.%s'
+    else:
+        require = '%s >= %s.%s, < %s.%s'
     require %= (name, major_version, minor_version,
         major_version, minor_version + 1)
     return require
 
 
 config = ConfigParser()
-config.read_file(open('tryton.cfg'))
+config.read_file(open(os.path.join(os.path.dirname(__file__), 'tryton.cfg')))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
@@ -33,80 +48,86 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'trytonar_account_arba'
 
-download_url = 'https://github.com/tryton-ar/account_arba/tree/%s.%s' % (
-    major_version, minor_version)
+url = 'https://github.com/tryton-ar/%s' % MODULE
+download_url = 'https://github.com/tryton-ar/%s/tree/%s.%s' % (
+    MODULE, major_version, minor_version)
 
-requires = []
+LINKS = {
+    'trytonar_account_ar': ('git+https://github.com/tryton-ar/'
+        'account_ar.git@%s.%s#egg=trytonar_account_ar-%s.%s' %
+        (major_version, minor_version, major_version, minor_version)),
+    'trytonar_account_retencion_ar': ('git+https://github.com/tryton-ar/'
+        'account_retencion_ar.git@%s.%s#egg=trytonar_account_retencion_ar-%s.%s' %
+        (major_version, minor_version, major_version, minor_version)),
+    }
+
+requires = ['M2Crypto>=0.22.3']
 for dep in info.get('depends', []):
-    if dep == 'account_ar':
-        requires.append(get_require_version('trytonar_%s' % dep))
-    elif not re.match(r'(ir|res)(\W|$)', dep):
-        requires.append(get_require_version('trytond_%s' % dep))
+    if not re.match(r'(ir|res)(\W|$)', dep):
+        module_name = '%s_%s' % (MODULE2PREFIX.get(dep, 'trytond'), dep)
+        requires.append(get_require_version(module_name))
+
 requires.append(get_require_version('trytond'))
-requires.append('M2Crypto>=0.22.3')
-requires.append('Pillow>=2.8.1')
-requires.append('httplib2')
-requires.append('pyafipws')
-requires.append('pysimplesoap')
 
 tests_require = [get_require_version('proteus')]
-dependency_links = [
-    'https://github.com/tryton-ar/account_ar/tarball/%s.%s#egg=trytonar_account_ar-%s.%s' \
-        % (major_version, minor_version, major_version, minor_version),
-    'https://github.com/reingart/pyafipws/tarball/py3k#egg=pyafipws',
-    'https://github.com/pysimplesoap/pysimplesoap/tarball/stable_py3k#egg=pysimplesoap',
-    ]
+dependency_links = list(LINKS.values())
 if minor_version % 2:
     dependency_links.append('https://trydevpi.tryton.org/')
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
     description='Tryton module that integrates ARBA (Argentina).',
     long_description=read('README'),
     author='tryton-ar',
-    url='https://github.com/tryton-ar/account_arba',
+    url=url,
     download_url=download_url,
-    package_dir={'trytond.modules.account_arba': '.'},
-    packages=[
-        'trytond.modules.account_arba',
-        'trytond.modules.account_arba.tests',
-        ],
+    project_urls={
+        "Bug Tracker": 'https://github.com/tryton-ar/%s/issues' % MODULE,
+        "Documentation": 'https://docs.tryton.org/',
+        "Forum": 'https://www.tryton.org/forum',
+        "Source Code": url,
+        },
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
+    packages=(
+        ['trytond.modules.%s' % MODULE]
+        + ['trytond.modules.%s.%s' % (MODULE, p) for p in find_packages()]
+        ),
     package_data={
-        'trytond.modules.account_arba': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
+        'trytond.modules.%s' % MODULE: (info.get('xml', []) + [
+            'tryton.cfg', 'view/*.xml', 'locale/*.po']),
         },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
         'Framework :: Tryton',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
         'Topic :: Office/Business :: Financial :: Accounting',
         ],
     license='GPL-3',
-    python_requires='>=3.4',
+    python_requires='>=3.7',
     install_requires=requires,
+    extras_require={
+        'test': tests_require,
+        },
     dependency_links=dependency_links,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    account_arba = trytond.modules.account_arba
-    """,
-    test_suite='tests',
-    test_loader='trytond.test_loader:Loader',
-    tests_require=tests_require,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     )
