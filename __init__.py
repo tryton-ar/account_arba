289f3658b3cceb1538775baa08bc326d34e90ec3
Adrián Bernardi <adrian@silix.com.ar>
2025-08-09 17:24:30 -0300

    Prepare release 7.0.1

--- __init__.py
+++ __init__.py
@@ -3,10 +3,11 @@
 # the full copyright notices and license terms.
 
 from trytond.pool import Pool
-
 from . import company
 from . import party
 from . import arba
+
+__all__ = ['register']
 
 
 def register():
